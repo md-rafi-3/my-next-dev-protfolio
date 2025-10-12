0062a5e93bd05e2b94c95c9da796e22bf2e6eaa9
md-rafi-3-4 <mdrafiislam9411@gmail.com>
2025-10-12 08:27:56 +0600

    updated

--- src/Components/CanvasLoader.jsx
+++ src/Components/CanvasLoader.jsx
@@ -1,60 +1,29 @@
-"use client";
-
-import React from "react";
 import { Html, useProgress } from "@react-three/drei";
 
 const CanvasLoader = () => {
   const { progress } = useProgress();
-
   return (
     <Html
-      as="div"
+      as='div'
       center
       style={{
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
         flexDirection: "column",
-        background: "transparent",
       }}
     >
-      {/* Loading spinner */}
-      <span
-        className="canvas-loader"
-        style={{
-          width: "40px",
-          height: "40px",
-          border: "4px solid rgba(255,255,255,0.3)",
-          borderTop: "4px solid #00C6FF",
-          borderRadius: "50%",
-          animation: "spin 1s linear infinite",
-        }}
-      />
-
-      {/* Loading percentage */}
+      <span className='canvas-loader'></span>
       <p
         style={{
-          fontSize: "14px",
+          fontSize: 14,
           color: "#F1F1F1",
-          fontWeight: "700",
-          marginTop: "12px",
-          letterSpacing: "1px",
+          fontWeight: 800,
+          marginTop: 40,
         }}
       >
-        {progress.toFixed(0)}%
+        {progress.toFixed(2)}%
       </p>
-
-      {/* Custom inline keyframes (to avoid external CSS dependency) */}
-      <style jsx>{`
-        @keyframes spin {
-          0% {
-            transform: rotate(0deg);
-          }
-          100% {
-            transform: rotate(360deg);
-          }
-        }
-      `}</style>
     </Html>
   );
 };
